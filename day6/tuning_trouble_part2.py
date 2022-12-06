e9c8cb167e6214389b9382e401e4f3b6aa4cd8d8
Oleh Zhovnuvatyi <oz@Olehs-MacBook-Pro.local>
2022-12-06 08:59:34 +0200

    day6 lite 6min exercise

---
+++ day6/tuning_trouble_part2.py
@@ -0,0 +1,19 @@
+def find_first_n_unique(data, n):
+    packet = []
+    for i, char in enumerate(data, start=1):
+        if char in packet:
+            del packet[0:packet.index(char)+1]
+        packet.append(char)
+        if len(packet) == n:
+            print(packet)
+            return i
+
+
+def main():
+    with open("input.txt", "r") as f:
+        data = f.read()
+    print(find_first_n_unique(data, 14))
+
+
+if __name__ == "__main__":
+    main()
